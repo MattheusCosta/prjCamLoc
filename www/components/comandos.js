2e8d850dad9ba213375c13ada7789f199e8a2e31
FylipePablo <fylipe5201pablo@hotmail.com>
2019-05-03 05:34:46 +0900

    Tirando foto com a cameraColocando a foto na tela inicial

--- www/components/comandos.js
+++ www/components/comandos.js
@@ -1,28 +1,46 @@
 // This is a JavaScript file
-
-$(document).on('click', '#camera', function(){
-  document.addEventListener("deviceready", onDeviceReady, false);
-  function onDeviceReady() {
-      console.log(navigator.camera);
-  }
-});
-
-
-function cameraCallback(imageData) {
-  var image = document.getElementById('myImage');
-  image.src = "data:image/jpeg;base64," + imageData;
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    console.log(navigator.camera);
 }
 
+$(document).on('click', '#camera', function(){
+  navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+  destinationType: Camera.DestinationType.FILE_URI });
 
-$(document).on('click', '#limpar', function(){
-  navigator.camera.cleanup(onSuccess, onFail);
-  function onSuccess() {
-      console.log("Camera cleanup success.")
+  function onSuccess(imageURI) {
+    var image = document.getElementById('myImage');
+    image.src = imageURI;
+    
+      function mapa(position){
+          L.mapquest.key = 'lYrP4vF3Uk5zgTiGGuEzQGwGIVDGuy24';
+
+          var map = L.mapquest.map('map', {
+          center: [position.coords.latitude, position.coords.longitude],
+          layers: L.mapquest.tileLayer('map'),
+          zoom: 15
+          });
+
+          L.marker([position.coords.latitude, position.coords.longitude], {
+              icon: L.mapquest.icons.marker(),
+              draggable: false
+            }).bindPopup('Denver, CO').addTo(map);
+
+            L.circle([position.coords.latitude, position.coords.longitude], { radius: 200 }).addTo(map);
+
+          map.addControl(L.mapquest.control());
+        };
+        navigator.geolocation.getCurrentPosition(mapa);
   }
 
   function onFail(message) {
-      alert('Failed because: ' + message);
+    alert('Failed because: ' + message);
   }
+});
+
+$(document).on('click', '#limpar', function(){
+  var image = document.getElementById('myImage');
+  image.src = ('imagens/cam.gif');
 });
 
 
